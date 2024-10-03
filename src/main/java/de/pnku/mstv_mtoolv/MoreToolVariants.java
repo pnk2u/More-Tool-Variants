bf8ffc99c4a384524ca347b35ba8d3dfebafe65f
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-10-03 13:30:52 +0200

    Initial Commit (1.21)

--- src/main/java/de/pnku/mstv_mtoolv/MoreToolVariants.java
+++ src/main/java/de/pnku/mstv_mtoolv/MoreToolVariants.java
@@ -20,7 +20,7 @@ public class MoreToolVariants implements ModInitializer {
 	}
 
 	public static ResourceLocation asId(String path) {
-		return new ResourceLocation(MOD_ID, path);
+		return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
 	}
 
 }
